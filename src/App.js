51e4259f9448d0d4cbc3662b55448c071bb2b270
Ilona Sellenberkova <ilona.sell@gmail.com>
2018-11-18 18:58:41 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
+// need import Flat component
+// and put here the variable flat => not to flat.js!!
 
 class App extends Component {
   render() {
+    const flat = {
+    "id": 145,
+    "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+    "price": 164,
+    "priceCurrency": "EUR",
+    "lat": 48.884211,
+    "lng": 2.346890
+  };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Flat flat={flat} />
       </div>
     );
   }
---
+++ src/components/flat.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './flat.css';
+
+// take temporaly from App.js
+
+// const flat = {
+//   name: 'By the Eiffel Tower',
+//   price: 100,
+//   priceCurrency: "$"
+// };
+// <Flat flat={flat} />
+// somewhere have info about flat in  the parent element,
+// passing to the components this way: <Flat flat={flat} />
+// better than to pass all props separatelly, good for API
+
+
+
+class Flat extends Component {
+  render() {
+
+    const title = this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
+
+    const style = {
+      // template literals ES6
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    };
+
+    return(
+      <div className="flat">
+        <div className="flat-picture" style={style}></div>
+        <div className="flat-title">
+          {title}
+        </div>
+      </div>
+      );
+  }
+}
+
+export default Flat;
+
+// we need to put some props from external world
+
+// import React from 'react';
+// ...
+// class Hello extends React.Component
