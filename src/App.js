80042d3b866ae2ae30f0b361504246cddd8317a7
Ilona Sellenberkova <ilona.sell@gmail.com>
2018-11-18 20:45:22 +0100

    using state with constructor, get rid of const flat and array flats

--- src/App.js
+++ src/App.js
@@ -5,26 +5,32 @@ import Flat from './components/flat';
 // and put here the variable flat => not to flat.js!!
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
   render() {
 
-    const flat = {
-    "id": 145,
-    "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-    "price": 164,
-    "priceCurrency": "EUR",
-    "lat": 48.884211,
-    "lng": 2.346890
-  };
+  //   const flat = {
+  //   "id": 145,
+  //   "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+  //   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+  //   "price": 164,
+  //   "priceCurrency": "EUR",
+  //   "lat": 48.884211,
+  //   "lng": 2.346890
+  // };
 
-  const flats = [flat, flat]
+  // const flats = [flat, flat]
 
     return (
       <div className="app">
         <div className="main">
           <div className="search"></div>
           <div className="flats">
-            {flats.map((flat) =>
+            {this.state.flats.map((flat) =>
               <Flat flat={flat} />
           )}
           </div>
